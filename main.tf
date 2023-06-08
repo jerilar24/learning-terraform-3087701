dfc93b17cc5d84679c151a8f2ad27983800a1c8a
jerilar24 <36524300+jerilar24@users.noreply.github.com>
2023-06-08 09:19:25 -0400

    correct subnet must declare on instance

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -45,7 +46,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
